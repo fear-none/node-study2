6e6640ed888438f4bfc4ca38e76084b0c76be158
eunho <jeh4916@gmail.com>
2023-09-29 21:43:58 +0900

    add template engine(pug)

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
+
+// view engine setup
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
 // middleware
 // GET home page
 app.get("/", (req, res) => {
   res.send("home page");
+});
+
+app.get("/write", (req, res) => {
+  res.render("write");
 });
 
 app.listen(3000, () => {
