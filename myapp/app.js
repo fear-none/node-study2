dcc040ac1ebe9c319243283b087d695a113f9db5
eunho <jeh4916@gmail.com>
2023-10-03 15:22:01 +0900

    add views

--- myapp/app.js
+++ myapp/app.js
@@ -5,6 +5,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
+// body parser set
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 // view engine setup
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
@@ -12,11 +16,18 @@ app.set("view engine", "pug");
 // middleware
 // GET home page
 app.get("/", (req, res) => {
-  res.send("home page");
+  res.render("main");
 });
 
 app.get("/write", (req, res) => {
   res.render("write");
+});
+
+app.post("/write", (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+
+  res.render("detail", {});
 });
 
 app.listen(3000, () => {
